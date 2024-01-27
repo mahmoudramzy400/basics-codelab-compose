bfa4ae7d63fbce3373cf5579fa3852bf7331a1f9
Mahmoud Ramzy <mahmoudramzy400@gmail.com>
2024-01-27 15:08:36 +0200

    Finish the lesson 3

--- app/src/main/java/com/devzza/basicscodelabcompose/ui/theme/Theme.kt
+++ app/src/main/java/com/devzza/basicscodelabcompose/ui/theme/Theme.kt
@@ -10,19 +10,28 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
+    surface = Blue,
+    onSurface = Navy,
+    primary = Navy,
+    onPrimary = Chartreuse,
+
     secondary = PurpleGrey80,
     tertiary = Pink80
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
+    surface = Blue,
+    onSurface = Color.White,
+    primary = LightBlue,
+    onPrimary = Navy,
+
     secondary = PurpleGrey40,
     tertiary = Pink40
 
@@ -41,7 +50,7 @@ private val LightColorScheme = lightColorScheme(
 fun BasicsCodelabComposeTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
